083bdc3063cbf882f5869d79f3570f0c96ac8b6f
ndvybios <70304367+ndvybios@users.noreply.github.com>
2022-10-25 23:13:01 +0700

    Update youtube_script.js

--- youtube_script.js
+++ youtube_script.js
@@ -1,7 +1,7 @@
-filter qc cuoi video youtube
+'''filter qc cuoi video youtube'''
 www.youtube.com##.ytp-ce-element
 
-hide control when full screen
+'''hide control when full screen'''
 youtube.com###movie_player > div.ytp-gradient-top
 youtube.com###movie_player > div.ytp-chrome-top
 youtube.com###movie_player > div.ytp-gradient-bottom
